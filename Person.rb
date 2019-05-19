e25f1280698c0c47a38b6d641ce28cbb9cc8e604
= <dvsocean@icloud.com>
2019-05-19 15:59:59 -0700

    added inheritance example

--- Person.rb
+++ Person.rb
@@ -1,20 +1,22 @@
-class Person
+require_relative "action"
 
-	@@max_age = 101
+class Person < Action
 
-	def initialize(id, name, number)
-		@person_id = id
+	@@max_age = 12 * 2 + 84
+
+	def initialize(age, name, number)
+		@person_age = age
 		@person_name = name
 		@person_number = number
 	end
 
 	def showDetails()
-		puts"Persons id --> #{@person_id}"
+		puts"Persons age --> #{@person_age}"
 		puts"Persons name --> #{@person_name}"
 		puts"Persons number --> #{@person_number}"
 	end
 
 	def showMaxAge()
-		puts("max age of any person is --> #{@@max_age}")
+		return @@max_age
 	end
 end
